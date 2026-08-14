026f4a523ae87538fa29ed8a0a18f538f3c5b0bf
Arafath <alarafath.info@gmail.com>
2026-08-14 20:21:27 +0600

    complete middleware.go to access middlewares

---
+++ backend/rest/middlewares/middleware.go
@@ -0,0 +1,13 @@
+package middlewares
+
+import "url-shortener/config"
+
+type Middlewares struct {
+	cnf *config.Config
+}
+
+func NewMiddlewares(cnf *config.Config) *Middlewares {
+	return &Middlewares{
+		cnf: cnf,
+	}
+}
