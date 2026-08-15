31428c82cb5b58e8a1db9253a75ba6d0367414e2
Arafath <alarafath.info@gmail.com>
2026-08-15 20:14:50 +0600

    added database migrations

--- backend/cmd/serve.go
+++ backend/cmd/serve.go
@@ -1,12 +1,24 @@
 package cmd
 
 import (
+	"fmt"
+	"os"
 	"url-shortener/config"
+	"url-shortener/infra/db"
 	"url-shortener/rest"
 )
 
 func Serve() {
 	cnf := config.GetConfig()
+
+	dbCon, err := db.NewConnection(cnf.DB)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	err = db.MigrateDB(dbCon, "./migrations")
 
 	server := rest.NewServer(cnf)
 
